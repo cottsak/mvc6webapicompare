1227e9e9ab84fc02ace79aeaefa1599db7f85abd
matt kocaj <mkocaj@hotmail.com>
2016-02-08 14:28:34 +0800

    switching to Owin

--- webapi2/SocksDrawer/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocksDrawer.WebApiApplication" Language="C#" %>
---
+++ webapi2/SocksDrawer/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using SocksDrawer;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace SocksDrawer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
