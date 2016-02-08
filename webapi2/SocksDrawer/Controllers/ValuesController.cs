c83f68414861ee69bebc89e7eaaa682a8ef2f886
matt kocaj <mkocaj@hotmail.com>
2016-02-08 14:35:25 +0800

    adding dummy service and wiring in Autofac for DI.

--- webapi2/SocksDrawer/Controllers/ValuesController.cs
+++ webapi2/SocksDrawer/Controllers/ValuesController.cs
@@ -9,9 +9,29 @@ namespace SocksDrawer.Controllers
 {
     public class ValuesController : ApiController
     {
+        private readonly IValuesGetter _valuesGetter;
+
+        public ValuesController(IValuesGetter valuesGetter)
+        {
+            _valuesGetter = valuesGetter;
+        }
+
         public object Get()
         {
-            return Json(new { header = "yo!", values = new[] { "val1", "val2" } });
+            return _valuesGetter.GetValues();
+        }
+    }
+
+    public interface IValuesGetter
+    {
+        string[] GetValues();
+    }
+
+    class ValuesGetter : IValuesGetter
+    {
+        public string[] GetValues()
+        {
+            return new[] { "val1", "val2" };
         }
     }
 }
--- webapi2/SocksDrawer/Startup.cs
+++ webapi2/SocksDrawer/Startup.cs
@@ -1,7 +1,11 @@
-﻿using System.Web.Http;
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
 using Microsoft.Owin;
 using Owin;
 using SocksDrawer;
+using SocksDrawer.Controllers;
 
 [assembly: OwinStartup(typeof(Startup))]
 
@@ -11,7 +15,36 @@ namespace SocksDrawer
     {
         public void Configuration(IAppBuilder app)
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            var builder = new ContainerBuilder();
+
+            // STANDARD WEB API SETUP:
+
+            // Get your HttpConfiguration. In OWIN, you'll create one
+            // rather than using GlobalConfiguration.
+            var config = new HttpConfiguration();
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Run other optional steps, like registering filters,
+            // per-controller-type services, etc., then set the dependency resolver
+            // to be Autofac.
+            builder.RegisterType<ValuesGetter>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            // OWIN WEB API SETUP:
+
+            // Register the Autofac middleware FIRST, then the Autofac Web API middleware,
+            // and finally the standard Web API middleware.
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
+
+            WebApiConfig.Register(config);
         }
     }
 }
