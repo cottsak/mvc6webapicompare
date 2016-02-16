12f1c24e870ce6c34b5799d3a8fbf9ec55b895ab
matt kocaj <mkocaj@hotmail.com>
2016-02-16 20:09:02 +0800

    adding back last of the business logic.

--- mvc6/src/SocksDrawer.Mvc6/Controllers/DrawerController.cs
+++ mvc6/src/SocksDrawer.Mvc6/Controllers/DrawerController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using Microsoft.AspNet.Mvc;
 using NHibernate;
 using NHibernate.Linq;
@@ -42,10 +43,11 @@ namespace SocksDrawer.Mvc6.Controllers
         {
             var newSocksPair = new SocksPair((SocksColour)Enum.Parse(typeof(SocksColour), newPairDto.Colour, ignoreCase: true));
 
-            //// yes, this is domain logic and shouldn't live in the controller
-            //if (_session.Query<SocksPair>().Count(p => p.Colour == SocksColour.White) == 6
-            //    && newSocksPair.Colour == SocksColour.White)
-            //    return Content(HttpStatusCode.Forbidden, "Maximum of 6 white pairs in the drawer at one time.");
+            // yes, this is domain logic and shouldn't live in the controller
+            if (_session.Query<SocksPair>().Count(p => p.Colour == SocksColour.White) == 6
+                && newSocksPair.Colour == SocksColour.White)
+                return new ContentResult
+                { StatusCode = (int?)HttpStatusCode.Forbidden, Content = "Maximum of 6 white pairs in the drawer at one time." };
 
             _session.Save(newSocksPair);
 
